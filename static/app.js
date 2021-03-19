8a7d90e7717d60d5b87f3d732781804cc2a93ce3
Tobias Kiertscher <dev@mastersign.de>
2021-03-19 13:30:27 +0000

    added map style selector

--- static/app.js
+++ static/app.js
@@ -1,15 +1,37 @@
-function setupMap() {
-    var map = L.map('map').setView([52.41563, 12.54754], 13);
+function basemap(style) {
+    var tileServerUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    if (style) {
+        tileServerUrl = 'https://tiles.mastersign.de/styles/' + style + '/{z}/{x}/{y}.png';
+    }
+    return L.tileLayer(tileServerUrl, {
+        attribution: attribution
+    });
+}
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+function updateMap(ctx, style) {
+    var map = ctx.map || (ctx.map = L.map('map').setView([52.41563, 12.54754], 13));
+    var mapStyle = ctx.mapStyle || 'initial';
 
-    L.marker([52.41563, 12.54754]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+    if (mapStyle !== style) {
+        ctx.mapStyle = style;
+        ctx.tileLayer = basemap(style);
+        map.eachLayer(function (l) { map.removeLayer(l); });
+        map.addLayer(ctx.tileLayer);
+    }
+}
+
+function setupMap(ctx) {
+    updateMap(ctx, $('#map-style').val());
+
+    $('#map-style').on('change', function (e) {
+        updateMap(ctx, $(e.target).val());
+    });
 }
 
 $(function () {
-    setupMap();
+    var ctx = {};
+    window.ctx = ctx;
+
+    setupMap(ctx);
 });
