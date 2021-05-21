43dda76c8c4fa61aa0ea46422ef8182ea2e5d8c7
Tobias Kiertscher <dev@mastersign.de>
2021-05-21 11:36:10 +0000

    set default locale for Vega and VegaLite

--- static/app.js
+++ static/app.js
@@ -1,3 +1,22 @@
+// set the local for Vega and VegaLite
+vega.defaultLocale(
+    {
+        "decimal": ",",
+        "thousands": ".",
+        "grouping": [3],
+        "currency": ["", "\u00a0€"]
+    },
+    {
+        "dateTime": "%A, der %e. %B %Y, %X",
+        "date": "%d.%m.%Y",
+        "time": "%H:%M:%S",
+        "periods": ["AM", "PM"],
+        "days": ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+        "shortDays": ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+        "months": ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+        "shortMonths": ["Jan", "Feb", "Mrz", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"]
+    }
+);
 function tileServerUrl(style) {
     return style
         ? window.cfg.styledTileServer + '/styles/' + style + '/{z}/{x}/{y}.png'
